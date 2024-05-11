2437434b2161e1249b2f06222649cfb44672a2eb
gangadhar <141738423+dudduvarigangadhar@users.noreply.github.com>
2024-05-11 17:37:10 +0530

    Update index.js

--- src/components/FiltersGroup/index.js
+++ src/components/FiltersGroup/index.js
@@ -3,35 +3,55 @@ import './index.css'
 const FiltersGroup = props => {
   const {categoryOptions, ratingsList} = props
 
+  //   return (
+  //     <ul className="filters-group-container">
+  //       <p className="category-content">Category</p>
+  //       {categoryOptions.map(eachItem => (
+  //         <li className="category-list-item" key={eachItem.categoryId}>
+  //           <button type="button" className="category-button">
+  //             {eachItem.name}
+  //           </button>
+  //         </li>
+  //       ))}
+
+  //       <p className="rating-content">Rating</p>
+  //       {ratingsList.map(eachItem => (
+  //         <li className="rating-list-container" key={eachItem.ratingId}>
+  //           <button type="button" className="rating-button">
+  //             <img
+  //               src={eachItem.imageUrl}
+  //               alt="rating"
+  //               className="rating-image"
+  //             />
+  //             <p className="rating-above">& up</p>
+  //           </button>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   )
+  const renderSearchInput = () => {
+      const {searchInput} = props 
+      return(
+          
+      )
+
+  }
+
+  const renderProductCategories = () => {}
+
+  const renderRatingsFiltersList = () => {
+
+  }
+
   return (
-    <ul className="filters-group-container">
-      <p className="category-content">Category</p>
-      {categoryOptions.map(eachItem => (
-        <li className="category-list-item" key={eachItem.categoryId}>
-          <button type="button" className="category-button">
-            {eachItem.name}
-          </button>
-        </li>
-      ))}
-
-      <p className="rating-content">Rating</p>
-      {ratingsList.map(eachItem => (
-        <li className="rating-list-container" key={eachItem.ratingId}>
-          <button type="button" className="rating-button">
-            <img
-              src={eachItem.imageUrl}
-              alt="rating"
-              className="rating-image"
-            />
-            <p className="rating-above">& up</p>
-          </button>
-        </li>
-      ))}
-
+    <div className="filters-group-container">
+      {renderSearchInput()}
+      {renderProductCategories()}
+      {renderRatingsFiltersList()}
       <button type="button" className="clear-button">
         Clear Filters
       </button>
-    </ul>
+    </div>
   )
 }
 
